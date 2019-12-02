93e54919fc6aab4bd992982c46d2cbb368605916
Karrol <zlscuwy@163.com>
2019-12-02 19:32:00 +0800

    2019/12/02张丽 提交 login

--- DigitalLibrary/login/admin.py
+++ DigitalLibrary/login/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from .models import Book, Borrowing, Reader
-from .models import *
+from .models import Reader
 
-admin.site.register(Book)
-admin.site.register(Borrowing)
 admin.site.register(Reader)
-admin.site.register(newsColumn_info)
-admin.site.register(article_info)
-admin.site.register(weekbook)
+
 
 admin.site.name = '图书馆信息管理'
 admin.site.site_header = '图书馆信息管理'
--- DigitalLibrary/login/views.py
+++ DigitalLibrary/login/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from django import forms
 from django.contrib import auth
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import Reader
+from .forms import LoginForm,RegisterForm,ResetPasswordForm
 
 # Create your views here.
 
 
 #用户登录（目前采用的电话登录方式）
 def user_login(request):
+    # 判断用户是否已经登录,会有自动登录的情况存在，主要是浏览器记录了账号密码
     if request.user.is_authenticated:
-        return HttpResponseRedirect('/')
-
+        return redirect('search:searchindex')
+    # 状态变量，用于在决定视图中渲染的提示消息
     state = None
-
+    # 获取表单数据
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        # 检验账号、密码是否正确匹配数据库中的某个用户
+        # 如果均匹配则返回这个 user 对象，authenticate()方法验证用户名称和密码是否匹配，如果是，则将这个用户数据返回
         user = auth.authenticate(username=username, password=password)
-
+        # 判断登录用户是否属于系统中已注册用户
         if user:
+            # 检验用户账号是否被激活，系统默认登录时，此语句将永远不会被执行
+            #当用户自行登录时，登录成功会跳转到用户个人中心中
             if user.is_active:
                 auth.login(request, user)
-                return HttpResponseRedirect('/')
+                return redirect('readerCenter:profile')
             else:
-                return HttpResponse(u'Your account is disabled.')
+                return HttpResponse(u'您的账户未激活！')
         else:
             state = 'not_exist_or_password_error'
 
@@ -34,10 +40,10 @@ def user_login(request):
         'state': state,
     }
 
-    return render(request, 'library:login', context)
+    return render(request, 'login/login.html', context)
 
 
-#用户注册（目前采用的电话注册方式）
+#用户注册（目前采用的电话登录方式）  (to do:转换注册方式)
 def user_register(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('/')
@@ -73,14 +79,14 @@ def user_register(request):
                     'state': state,
                     'registerForm': registerForm,
                 }
-                return render(request, 'library/register.html', context)
+                return render(request, 'login/register.html', context)
 
     context = {
         'state': state,
         'registerForm': registerForm,
     }
 
-    return render(request, 'library/register.html', context)
+    return render(request, 'login/register.html', context)
 
 
 #用户修改验证码
@@ -108,7 +114,7 @@ def set_password(request):
         'resetPasswordForm': ResetPasswordForm(),
     }
 
-    return render(request, 'library/set_password.html', context)
+    return render(request, 'login/set_password.html', context)
 
 
 #用户登出
